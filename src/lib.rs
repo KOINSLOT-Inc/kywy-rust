3cd87101bb4dbd3c4ced26541a7047d273d9044a
jmlannan <jmlannan@gmail.com>
2025-04-02 03:21:37 -0400

    Added button polling library and example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
 #![no_std]
 pub mod battery;
-pub mod buttons;
 pub mod display;
 pub mod sdcard;
 
 #[macro_use]
 pub mod macros;
+
+#[cfg(all(feature = "button-async", feature = "button-poll"))]
+compile_error!("You cannot enable both `button-async` and `button-poll`. Choose one.");
+
+#[cfg(feature = "button-async")]
+pub mod button_async;
+
+#[cfg(feature = "button-poll")]
+pub mod button_poll;
