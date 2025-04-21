3b541ecfa608bdafb12b2d0030cd68ee3a4e7bca
jmlannan <jmlannan@gmail.com>
2025-04-20 20:14:50 -0400

    removed broken renderer code

--- examples/sprite_test.rs
+++ examples/sprite_test.rs
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2025 KOINSLOT Inc.
-//
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 //! This example demonstrates the use of the `Sprite` struct from the `kywy` crate.
