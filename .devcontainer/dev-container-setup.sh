0d6b11a05b5fa06eca079b20ba4d43fab470c835
Joe <3395054+Jmlannan@users.noreply.github.com>
2025-04-21 18:38:03 -0400

    Update dev-container-setup.sh
    Signed-off-by: Joe <3395054+Jmlannan@users.noreply.github.com>

--- .devcontainer/dev-container-setup.sh
+++ .devcontainer/dev-container-setup.sh
@@ -4,3 +4,10 @@
 sudo apt-get update
 sudo apt-get install -y imagemagick
 sudo apt-get install -y elf2uf2
+
+# Clean up
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
+# Let user know we are ready
+echo "Container Setup Complete"
