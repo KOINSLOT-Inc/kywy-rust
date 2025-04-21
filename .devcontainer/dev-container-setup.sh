b4a7be10b0d0f91035f8542858e7a6451855dc70
Joe <3395054+Jmlannan@users.noreply.github.com>
2025-04-21 18:50:32 -0400

    Update dev-container-setup.sh
    Signed-off-by: Joe <3395054+Jmlannan@users.noreply.github.com>

--- .devcontainer/dev-container-setup.sh
+++ .devcontainer/dev-container-setup.sh
@@ -3,11 +3,15 @@
 # Update and install common tools
 sudo apt-get update
 sudo apt-get install -y imagemagick
-sudo apt-get install -y elf2uf2
+cargo install elf2uf2-rs
 
 # Clean up
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
+cargo clean
+
+# Set build target to thumbv6m-none-eabi
+echo '[build]\\ntarget = \"thumbv6m-none-eabi\"' > .cargo/config.toml"
 
 # Let user know we are ready
 echo "Container Setup Complete"
