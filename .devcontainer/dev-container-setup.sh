b559df21fbc2d2c5e25291f8db23efe3762638eb
Joe <3395054+Jmlannan@users.noreply.github.com>
2025-04-21 19:30:22 -0400

    Update dev-container-setup.sh
    Signed-off-by: Joe <3395054+Jmlannan@users.noreply.github.com>

--- .devcontainer/dev-container-setup.sh
+++ .devcontainer/dev-container-setup.sh
@@ -7,13 +7,10 @@ sudo apt-get install -y pkg-config libudev-dev # rust/cargo dependencies
 
 cargo install elf2uf2-rs # elf2uf2 for producing uf2 files
 
-# Clean up
+# Clean up to save space
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
 cargo clean
-
-# Set build target to thumbv6m-none-eabi
-echo "'[build]\\ntarget = \"thumbv6m-none-eabi\"' > .cargo/config.toml"
 
 # Let user know we are ready
 echo "Container Setup Complete"
