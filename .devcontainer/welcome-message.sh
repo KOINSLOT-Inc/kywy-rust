1c8d183955fd7cf54abdc260dfe7e04bac932dd5
Joe <3395054+Jmlannan@users.noreply.github.com>
2025-04-22 12:19:44 -0400

    Update welcome-message.sh
    Signed-off-by: Joe <3395054+Jmlannan@users.noreply.github.com>

--- .devcontainer/welcome-message.sh
+++ .devcontainer/welcome-message.sh
@@ -1,1 +1,7 @@
-echo -e "Welcome to the KOINSLOT Arduino devcontainer"
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 2025 KOINSLOT Inc.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+echo -e "Welcome to the KOINSLOT kywy-rust devcontainer"
