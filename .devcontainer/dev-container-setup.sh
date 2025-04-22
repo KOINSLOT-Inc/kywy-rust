8f4f3641b32b7c397d535497b354d61ee6bf56d5
Joe <3395054+Jmlannan@users.noreply.github.com>
2025-04-21 23:33:43 -0400

    Update dev-container-setup.sh
    Signed-off-by: Joe <3395054+Jmlannan@users.noreply.github.com>

--- .devcontainer/dev-container-setup.sh
+++ .devcontainer/dev-container-setup.sh
@@ -2,7 +2,7 @@
 
 # Setup environment and add scripts to path
 chmod +x .devcontainer/scripts/*.sh
-export PATH="${PATH}:/workspaces/kywy-rust/.devcontainer/scripts"
+echo 'export PATH="$PATH:/workspaces/kywy-rust/scripts"' >> ~/.bashrc
 
 # Update and install common tools
 sudo apt-get update
